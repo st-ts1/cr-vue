d78ff5bf61721ade87a87322693c2469f387560e
mio3io <2270244+mio3io@users.noreply.github.com>
2018-05-24 04:39:35 +0900

    ch8コンポーネント

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -5,6 +5,9 @@ import PageInfo from './components/page-info.vue'
 import DemoBlock from './components/demo-block.vue'
 import ExerciseBlock from './components/exercise-block.vue'
 
+import GuideCh8S43SrcApp from './components/guide/ch8/s43/src/App.vue'
+import GuideCh8S44SrcApp from './components/guide/ch8/s44/src/App.vue'
+
 export default ({
   Vue
 }) => {
@@ -13,4 +16,9 @@ export default ({
   Vue.component('page-info', PageInfo)
   Vue.component('demo-block', DemoBlock)
   Vue.component('exercise-block', ExerciseBlock)
+
+  // 非同期コンポーネントが読み込み失敗する対策
+  Vue.component('GuideCh8S43SrcApp', GuideCh8S43SrcApp)
+  Vue.component('GuideCh8S44SrcApp', GuideCh8S44SrcApp)
+
 }
