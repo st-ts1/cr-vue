a95de83e0a9f12fc02cec7eff0ebb7af3c76112e
mio3io <2270244+mio3io@users.noreply.github.com>
2019-03-03 01:14:33 +0900

    ads追加

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -4,6 +4,7 @@ import CodeCaption from './components/code-caption.vue'
 import PageInfo from './components/page-info.vue'
 import DemoBlock from './components/demo-block.vue'
 import ExerciseBlock from './components/exercise-block.vue'
+import GoogleAds from './components/google-ads.vue'
 
 import GuideCh8S43SrcApp from './components/guide/ch8/s43/src/App.vue'
 import GuideCh8S44SrcApp from './components/guide/ch8/s44/src/App.vue'
@@ -16,6 +17,7 @@ export default ({
   Vue.component('page-info', PageInfo)
   Vue.component('demo-block', DemoBlock)
   Vue.component('exercise-block', ExerciseBlock)
+  Vue.component('google-ads', GoogleAds)
 
   // 非同期コンポーネントが読み込み失敗する対策
   Vue.component('GuideCh8S43SrcApp', GuideCh8S43SrcApp)
